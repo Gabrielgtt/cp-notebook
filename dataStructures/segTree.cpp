bfaf2f783eb6065465d792934a804fe5b9b57371
Gabrielgtt <gabrielgtt@hotmail.com>
2019-07-31 20:55:42 -0300

    sparseTable e segTree

---
+++ dataStructures/segTree.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#define MAXN 1000000
+#define INF 1e9
+using namespace std;
+
+// Segtree de maximo
+int n;
+int seg[MAXN];
+
+int query(int ql, int qr, int l = 0, int r = n-1, int node = 1) {
+	if (qr < l || r < ql) return -INF;
+	if (ql <= l && r <= qr) return seg[node];
+	int mid = (l+r)>>1;
+	int left = query(ql, qr, l, mid, node*2);		
+	int right = query(ql, qr, mid+1, r, node*2+1);		
+	return max(left, right);
+}
+
+void update(int idx, int valor, int l = 0, int r = n-1, int node = 1) {
+	if (l == r) {
+		seg[node] = valor;
+	} else {
+		int mid = (l+r)>>1;
+		if (idx <= mid) update(idx, valor, l, mid, node*2);
+		else update(idx, valor, mid+1, r, node*2+1);
+		seg[node] = max(seg[node*2], seg[node*2+1]);
+	}
+}
+
+void build(int array[], int l = 0, int r = n-1, int node = 1) {
+	if (l == r) {
+		seg[node] = array[l];
+	} else {
+		int mid = (l+r)>>1;
+		build(array, l, mid, node*2);
+		build(array, mid+1, r, node*2+1);
+		seg[node] = max(seg[node*2], seg[node*2+1]);
+	}
+}
---
+++ dataStructures/sparseTable.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define MAXN 600000
+#define MAXLOG 22
+#define INF 1e9
+using namespace std;
+
+// Sparse table de min
+
+int n, logs[MAXN];
+struct sparseTable {
+	int table[MAXLOG][MAXN];
+
+	void build(int array[]) {
+		logs[0] = logs[1] = 0;
+		for (int i = 2; i < MAXN; i++) logs[i] = logs[i/2]+1;
+		for (int i = 0; i < n; i++) table[0][i] = array[i];
+		for (int j = 1; j < MAXLOG; j++) {
+			for (int i = 0; i + (1<<j) <= n; i++) {
+				table[j][i] = min(table[j-1][i], table[j-1][i+(1<<(j-1))]);
+			}
+		}
+	}
+
+	int query(int l, int r) {
+		if (l > r) swap(l, r);
+		return min(table[logs[r-l]][l], table[logs[r-l]][r - (1 << logs[r-l]) + 1]);
+	}
+} tb;
