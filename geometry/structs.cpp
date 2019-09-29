0eb7e25fd26da915bef953d703480b6d248c299b
Gabrielgtt <gabrielgtt@hotmail.com>
2019-09-29 09:21:19 -0300

    Modificando PI

--- geometry/structs.cpp
+++ geometry/structs.cpp
@@ -3,54 +3,54 @@
 using namespace std;
 
 const double EPS = 1e-9;
-const double PI = 3.141592653589793;
+const double PI = acos(-1);
 
 typedef long long T;
 
 ll gcd(ll a, ll b) { return b == 0 ? a : gcd(b, a%b); }
 
-struct Point {
+struct Pt {
 	T x, y;
-	Point(T a=0, T b=0) : x(a), y(b) {}
-	bool operator == (const Point& p) const { return p.x == x && p.y == y; }
+	Pt(T a=0, T b=0) : x(a), y(b) {}
+	bool operator == (const Pt& p) const { return p.x == x && p.y == y; }
 
-	bool operator < (const Point& p) const { return x == p.x ? y < p.y : x < p.x; }
+	bool operator < (const Pt& p) const { return x == p.x ? y < p.y : x < p.x; }
 
-	Point operator - (const Point& p) const { return {x - p.x, y - p.y}; }
+	Pt operator - (const Pt& p) const { return {x - p.x, y - p.y}; }
 	
-	Point operator + (const Point& p) const { return {x + p.x, y + p.y}; }
+	Pt operator + (const Pt& p) const { return {x + p.x, y + p.y}; }
 	
-	Point operator / (const T c) const { return {x / c, y / c}; }
+	Pt operator / (const T c) const { return {x / c, y / c}; }
 	
-	Point operator * (const T c) const { return {x * c, y * c}; }
+	Pt operator * (const T c) const { return {x * c, y * c}; }
 
-	Point vetor(const Point& o) const { return o - (*this); }
+	Pt vetor(const Pt& o) const { return o - (*this); }
 
-	T dot(const Point& b) const { return (x*b.x) + (y*b.y); }
+	T dot(const Pt& b) const { return (x*b.x) + (y*b.y); }
 	
 	T sq() const { return (x*x) + (y*y); }
 
-	double dist(const Point& b) { return sqrt((b - *this).sq()); }
+	double dist(const Pt& b) { return sqrt((b - *this).sq()); }
 
-	T cross(const Point& b) const { return (x*b.y) - (y*b.x); }
+	T cross(const Pt& b) const { return (x*b.y) - (y*b.x); }
 
 	// ccw !	
-	T cross(const Point& a, const Point& b) const { return (a - *this).cross(b - *this); }
+	T cross(const Pt& a, const Pt& b) const { return (a - *this).cross(b - *this); }
 	
-	double angle(Point& a, Point& b) const {
+	double angle(Pt& a, Pt& b) const {
 		a = vetor(a), b = vetor(b);
 		return acos(1.0*a.dot(b) / sqrt(1.0*a.dot(a)*b.dot(b)));
 	}
 };
 
-Point perp(Point p) {return {-p.y, p.x};}
+Pt perp(Pt p) {return {-p.y, p.x};}
 
-Point circumCenter(Point a, Point b, Point c) {
+Pt circumCenter(Pt a, Pt b, Pt c) {
 	b = b-a, c = c-a;
 	return a + (perp(b*c.sq() - c*b.sq()) / b.cross(c)) / 2;
 }
 
-ostream& operator<< (ostream& os, Point p) {
+ostream& operator<< (ostream& os, Pt p) {
 	return os << "("<< p.x << "," << p.y << ")";
 }
 
@@ -62,7 +62,7 @@ struct Line {
 	T A, B, C;
 
 	Line() {}
-	Line(const Point& de, const Point& pa) {
+	Line(const Pt& de, const Pt& pa) {
 		A = de.y - pa.y;
 		B = pa.x - de.x;
 		C = -A * de.x - B * de.y;
@@ -70,52 +70,65 @@ struct Line {
 };
 
 
+bool mORe(double a, double b) {
+	return a <= b || fabs(a - b) <= EPS;
+}
+
 // ----------------------------------------------------------------------------- 
 struct Seg {
-	Point A, B;
+	Pt A, B;
 
 	Seg() {}	
-	Seg(Point a, Point b) : A(a), B(b) { if (B < A) swap(A, B); }
+	Seg(Pt a, Pt b) : A(a), B(b) { if (B < A) swap(A, B); }
 
 	T numIntegerPoints() { return gcd(abs(A.x - B.x), abs(A.y - B.y)) + 1LL; }
 
-	bool in(const Point& pt) {
-		return min(A.x, B.x) <= pt.x 
-			&& pt.x <= max(A.x, B.x) 
-			&& min(A.y, B.y) <= pt.y 
-			&& pt.y <= max(A.y, B.y) && pt.cross(A, B) == 0;
+
+	bool inDouble(const Pt& Pt) {
+		return mORe(min(A.x, B.x), Pt.x) 
+			&& mORe(Pt.x, max(A.x, B.x)) 
+			&& mORe(min(A.y, B.y), Pt.y)
+			&& mORe(Pt.y, max(A.y, B.y));
 	}
 
-	bool inter(Seg& o, Point& res){
+
+	bool in(const Pt& Pt) {
+		return min(A.x, B.x) <= Pt.x 
+			&& Pt.x <= max(A.x, B.x) 
+			&& min(A.y, B.y) <= Pt.y 
+			&& Pt.y <= max(A.y, B.y);
+	}
+
+	bool inter(Seg& o, Pt& res){
 		Line l1 = Line(A, B), l2 = Line(o.A, o.B);
 
-		T dx = Point(l1.C, l1.B).cross(Point(l2.C, l2.B));
-		T dy = Point(l1.A, l1.C).cross(Point(l2.A, l2.C));
-		T d = Point(l1.A, l1.B).cross(Point(l2.A, l2.B));
+		T dx = Pt(l1.C, l1.B).cross(Pt(l2.C, l2.B));
+		T dy = Pt(l1.A, l1.C).cross(Pt(l2.A, l2.C));
+		T d = Pt(l1.A, l1.B).cross(Pt(l2.A, l2.B));
 
 		if (d == 0 || (dx % d != 0) || (dy % d != 0)) return false;
 		
-		res = Point(-dx / d, -dy / d);
+		res = Pt(-dx / d, -dy / d);
 		
-		if (!in(res) || !o.in(res)) return false;
+		if (!in(res) || !o.in(res)) return false; // trocar para inDouble se estiver usando Double
 		return true;
 	}
 }; 
-// --------------------------------------------------------------------- 20959294248
+// --------------------------------------------------------------------- 
 
 // --------------------------------------------------------------------- 
 struct Poly {
-	vector <Point> pts;
+	vector <Pt> pts;
 
-	Poly(Point* arrps, int size){
+	Poly(Pt* arrps, int size){
 		for (int i=0; i<size; i++){
 			pts.emplace_back(arrps[i]);
 		}
 		pts.emplace_back(arrps[0]);
 	}
 
-	vector <Point> convexHull() {
-		vector <Point> chain;
+	vector <Pt> convexHull() {
+		vector <Pt> chain;
 		pts.pop_back();
 		sort(pts.begin(), pts.end());
 			
@@ -139,17 +152,17 @@ struct Poly {
 
 	bool tiraConvexHull() {
 		if (pts.size() < 3) return false;
-		map <Point, bool> vis;
-		vector <Point> hull = convexHull();
-		for (Point p : hull) vis[p] = true;
-		vector <Point> resto;
-		for (Point p : pts) if (!vis[p]) resto.emplace_back(p);
+		map <Pt, bool> vis;
+		vector <Pt> hull = convexHull();
+		for (Pt p : hull) vis[p] = true;
+		vector <Pt> resto;
+		for (Pt p : pts) if (!vis[p]) resto.emplace_back(p);
 		if (resto.size() > 0) resto.emplace_back(resto[0]);
 		pts = resto;
 		return true;
 	}
 
-	bool inside(Point p){ // TESTAR DEPOIS DE MUDAR ANGLE
+	bool inside(Pt p){ // TESTAR DEPOIS DE MUDAR ANGLE
 		if (pts.size() == 0) return false;
 		double sum = 0;
 		for (int i=0; i<pts.size()-1; i++){
@@ -186,7 +199,7 @@ struct Poly {
 
 int main() {
 	// TESTES
-	Point a{0, 0}, b{1, 0}, c{0, 1};
+	Pt a{0, 0}, b{1, 0}, c{0, 1};
 	double angulo = a.angle(c, b) * 180.0 / PI;
 	assert(angulo == 90.0);
 	cout << a - b << endl;
