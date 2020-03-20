24795cfd911d71bde112bda88c29b584a994746e
Gabrielgtt <gabrielgtt@hotmail.com>
2020-03-20 11:57:30 -0300

    Update Sparse table to use template

--- dataStructures/sparseTable.cpp
+++ dataStructures/sparseTable.cpp
@@ -4,35 +4,33 @@
 #define INF 1e9
 using namespace std;
 
-// Sparse table de min
 
-int n, logs[MAXN];
 // ------------------------------------------------------------------------------ 
-struct SparseTable {
-	int table[MAXLOG][MAXN];
+int logs[MAXN];
 
-	void build(int array[]) {
+template <typename T>
+struct St {
+	vector<vector<T>> table;
+	St(T arr[], int n) {
 		logs[0] = logs[1] = 0;
-		for (int i = 2; i < MAXN; i++) logs[i] = logs[i/2]+1;
-		for (int i = 0; i < n; i++) table[0][i] = array[i];
-		for (int j = 1; j < MAXLOG; j++) {
-			for (int i = 0; i + (1<<j) <= n; i++) {
+		for (int i=2; i<n; i++) logs[i] = logs[i/2]+1;
+		int lo = logs[n-1]+1;
+		table.resize(lo, vector<T>(n));
+		for (int i=0; i<n; i++) table[0][i] = arr[i];
+		for (int j=1; j<lo; j++)
+			for (int i=0; i+(1<<j)<=n; i++)
 				table[j][i] = min(table[j-1][i], table[j-1][i+(1<<(j-1))]);
-			}
-		}
 	}
-
-	int query(int l, int r) {
+	T query(int l, int r) {
 		if (l > r) swap(l, r);
 		return min(table[logs[r-l]][l], table[logs[r-l]][r - (1 << logs[r-l]) + 1]);
 	}
-} tb;
+};
 // ------------------------------------------------------------------------------ 14631003288
 
 int main() {
 	int arr[10] = {1, 2, 3, -6, 1, 2, -2, 3, 4, 5};
-	n = 10;
-	tb.build(arr);
+	St <int> tb(arr, 10);
 	assert(tb.query(1, 6) == -6);
 	assert(tb.query(0, 2) == 1);
 	assert(tb.query(7, 9) == 3);
