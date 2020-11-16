fdc94d9d74d86419f263fb7829d00dadf9776d32
Gabrielgtt <gabrielgtt@hotmail.com>
2020-11-16 20:02:55 -0300

    Improve convex hull

--- geometry/cp-geo.cpp
+++ geometry/cp-geo.cpp
@@ -91,31 +91,28 @@ struct line {
 }
 
 void convex_hull(vector<pt>& a) {
-    auto cmp = [&](pt a, pt b) { return a.x < b.x || (a.x == b.x && a.y < b.y); };
-    auto ccw = [&](pt a, pt b, pt c) { return a.x*(b.y-c.y)+b.x*(c.y-a.y)+c.x*(a.y-b.y) > 0; };
-    auto cw = [&](pt a, pt b, pt c) { return a.x*(b.y-c.y)+b.x*(c.y-a.y)+c.x*(a.y-b.y) < 0; };
-
     if (a.size() == 1) return;
-    sort(a.begin(), a.end(), cmp);
+    sort(a.begin(), a.end(), [&](pt a, pt b) { 
+        return a.x < b.x || (a.x == b.x && a.y < b.y); 
+    });
     pt p1 = a[0], p2 = a.back();
     vector<pt> up, down;
     up.push_back(p1);
     down.push_back(p1);
-    for (int i = 1; i < (int) a.size(); i++) {
-        if (i == a.size() - 1 || cw(p1, a[i], p2)) {
-            while (up.size() >= 2 && !cw(up[up.size()-2], up[up.size()-1], a[i]))
+    for (int i = 1; i < (int)a.size(); i++) {
+        if (i == a.size() - 1 || orient(p1, a[i], p2) < 0) {
+            while (up.size() >= 2 && orient(up[up.size()-2], up[up.size()-1], a[i]) >= 0)
                 up.pop_back();
             up.push_back(a[i]);
         }
-        if (i == a.size() - 1 || ccw(p1, a[i], p2)) {
-            while(down.size() >= 2 && !ccw(down[down.size()-2], down[down.size()-1], a[i]))
+        if (i == a.size() - 1 || orient(p1, a[i], p2) > 0) {
+            while(down.size() >= 2 && orient(down[down.size()-2], down[down.size()-1], a[i]) <= 0)
                 down.pop_back();
             down.push_back(a[i]);
         }
     }
-
     a.clear();
-    for (int i = 0; i < (int) up.size(); i++)
+    for (int i = 0; i < (int)up.size(); i++)
         a.push_back(up[i]);
     for (int i = down.size() - 2; i > 0; i--)
         a.push_back(down[i]);
